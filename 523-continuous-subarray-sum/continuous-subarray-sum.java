91205de066bccd9f137117b100c43c990bab3870
Karan <karanverma07320@gmail.com>
2026-02-18 20:23:39 +0530

    Time: 23 ms (94.63%) | Memory: 140 MB (17.22%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int n=nums.length;
+         map.put(0,-1);
+        int prefixSum=0;
+        for(int i=0;i<n;i++){
+         prefixSum +=nums[i];
+        int remainder=prefixSum % k;
+        if(map.containsKey(remainder)){
+            if(i-map.get(remainder)>=2){
+                return true;
+            }
+        }
+        else{
+            map.put(remainder,i);
+        }
+    }
+    return false;
+}
+}
