e94f29eebddd8c71bc37f6eb5de7450296220fdf
Karan <karanverma07320@gmail.com>
2026-02-25 12:42:07 +0530

    Time: 3 ms (53.12%) | Memory: 51.2 MB (39.13%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,46 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        HashMap<Integer,Integer>map =new HashMap<>();
-    int n=nums.length;
-    int count=0;
-    int current_sum=0;
-    map.put(0,1);
-    for(int i=0;i<n;i++){
-        current_sum+=nums[i];
+    public int numSubarraysWithSum(int[] nums, int k) {
+          int i=0;
+        int n=nums.length;
+        int j=0;
+        int count=0;
+        int zerocount=0;
+        int curr_sum=0;
+        while(j<n){
+            curr_sum+=nums[j];
+            while(i<j && (curr_sum>k || nums[i]==0)){
+                if(nums[i]==0){
+                    zerocount++;
+                }
+                else{
+                    zerocount=0;
+                }
+                curr_sum-=nums[i];
+                i++;
+            }
+            if(curr_sum==k){
+                count+=1+zerocount;
+            }
+            j++;
+
+        }
+
+        return count;
+      
+    // HashMap<Integer,Integer>map =new HashMap<>();
+    // int n=nums.length;
+    // int count=0;
+    // int current_sum=0;
+    // map.put(0,1);
+    // for(int i=0;i<n;i++){
+    //     current_sum+=nums[i];
     
-    if (map.containsKey(current_sum-goal)){
-    count+=map.get(current_sum-goal);
-    }
-    map.put(current_sum,map.getOrDefault(current_sum,0)+1);
-    }
-    return count;
+    // if (map.containsKey(current_sum-goal)){
+    // count+=map.get(current_sum-goal);
+    // }
+    // map.put(current_sum,map.getOrDefault(current_sum,0)+1);
+    // }
+    // return count;
 }
-}
+}  
     
