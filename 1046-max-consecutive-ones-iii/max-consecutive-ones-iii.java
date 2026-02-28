fb8ad8eb10b4507abb223d5e0b528ed1207bd33d
Karan <karanverma07320@gmail.com>
2026-02-28 08:10:22 +0530

    Time: 4 ms (47.57%) | Memory: 51.9 MB (94.34%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,28 +1,53 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int i=0;
-        int j=0;
-        int curr_sum=0;
-        int ans=0;
-        int n=nums.length;
-        int zero=0;
-        for(int a=0;a<n;a++){
-            if(nums[a]==0){
-                zero++;
-            }
-        }
-        if(zero<k) return n;
-        while(j<n){
-            curr_sum+=nums[j];
-            if(j-i+1-curr_sum>k){
+    //     int n=nums.length;
+    // int i=0;
+    // int j=0;
+    // int ans=0;
+    // int curr_sum=0;
+    // int count=0;
+    // for(int a =0;a<n;a++){
+    //    if( nums[a]==0){
+    //     zero++;
+    //    }
+    // }
+    // if(zero<k) return n;
+    // while(j<n){
+    //     if(j-i+1-curr_sum>k){
+    //         curr_sum-=nums[i];
+    //         i++;
+    //     }
+    //     else if(j-i+1-curr_sum==k){
+    //         ans=Math.max(ans,j-i+1);
+    //     }
+    //     j++;
+    // }
+    // return ans;
+    // }
+    // }
+    // class Solution {
+    // public int longestOnes(int[] nums, int k) {
+
+        int i = 0, j = 0;
+        int ans = 0;
+        int curr_sum = 0;
+        int n = nums.length;
+
+        while (j < n) {
+
+            curr_sum += nums[j];   // count 1s
+
+            while (j - i + 1 - curr_sum > k) {
                 curr_sum -= nums[i];
                 i++;
             }
-            else if(j-i+1-curr_sum==k){
-                ans=Math.max(ans,j-i+1);
-            }
+
+            ans = Math.max(ans, j - i + 1);
+
             j++;
-            }
-    return ans;
+        }
+
+        return ans;
     }
 }
+    
