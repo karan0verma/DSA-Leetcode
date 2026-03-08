88f7ee48788358ee62558457fb8f83909403f247
Karan <karanverma07320@gmail.com>
2026-03-08 18:20:20 +0530

    Time: 5 ms (16.44%) | Memory: 44.5 MB (97.50%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,39 +1,68 @@
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         int n= nums2.length;
+//         HashMap<Integer,Integer>map= new HashMap<>();
+//         Stack < Integer> stack = new Stack<>();
+//         for( int i=n-1;i>=0;i--)
+//         {
+//             int current=nums2[i];
+//             if(stack.isEmpty())
+//             {
+//                 map.put(current ,-1);
+//             }
+//             else if(!stack.isEmpty() && stack.peek()>current)
+//             {
+//                 map.put(current , stack.peek());
+//             }
+//             else if(!stack.isEmpty() && stack.peek()<=current)
+//             {
+//                 while(!stack.isEmpty() && stack.peek()<=current)
+//                 {
+//                     stack.pop();
+//                 }
+//                 if(stack.isEmpty())
+//                 {
+//                     map.put(current ,-1);
+//                 }
+//                 else {
+//                     map.put(current, stack.peek());
+//                 }
+//             }
+//             stack.push(current);
+//         }
+//         int[] finalans=new int[nums1.length];
+//         for(int i=0;i<nums1.length;i++){
+//             finalans[i]=map.get(nums1[i]);
+//         }
+//         return finalans;
+//     }
+// }
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n= nums2.length;
-        HashMap<Integer,Integer>map= new HashMap<>();
-        Stack < Integer> stack = new Stack<>();
-        for( int i=n-1;i>=0;i--)
+        
+        int[] ans = new int[nums1.length] ;
+        for(int i = 0 ; i < nums1.length ; i++)
         {
-            int current=nums2[i];
-            if(stack.isEmpty())
-            {
-                map.put(current ,-1);
-            }
-            else if(!stack.isEmpty() && stack.peek()>current)
-            {
-                map.put(current , stack.peek());
-            }
-            else if(!stack.isEmpty() && stack.peek()<=current)
-            {
-                while(!stack.isEmpty() && stack.peek()<=current)
-                {
-                    stack.pop();
-                }
-                if(stack.isEmpty())
-                {
-                    map.put(current ,-1);
-                }
-                else {
-                    map.put(current, stack.peek());
-                }
-            }
-            stack.push(current);
+            ans[i] = -1 ;
         }
-        int[] finalans=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            finalans[i]=map.get(nums1[i]);
+        for(int i = 0 ;  i < nums1.length ; i++)
+        {
+            for(int j = 0 ; j < nums2.length ; j++)
+            {
+                if(nums1[i] == nums2[j])
+                {
+                    //next larger find karo
+                    for(int k = j+1 ; k < nums2.length ; k++)
+                    {
+                        if(nums2[j] < nums2[k])
+                        {
+                            ans[i] = nums2[k] ;
+                            break ;
+                        }
+                    }
+                }
+            }
         }
-        return finalans;
+        return ans ;
     }
 }
