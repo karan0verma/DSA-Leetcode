c5e3b7fcf8a22c03f20d2b09d362e73d7b0f2e27
Karan <karanverma07320@gmail.com>
2026-06-12 19:29:51 +0530

    Time: 1548 ms (9.35%) | Memory: 48.7 MB (60.12%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,35 +1,51 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer>map =new HashMap<>();
-        int n=nums.length;
         int count=0;
-          int current_sum=0;
-        map.put(0,1);
-        for(int i=0;i<n;i++){
-            current_sum+= nums[i];
-    
-        if(map.containsKey(current_sum-k)){
-            count+=map.get(current_sum-k);
+        for(int i=0;i<nums.length;i++){
+           int  sum=0;
+        
+        for(int j=i;j<nums.length;j++){
+            sum=sum+nums[j];
+            if(sum==k){
+                count++;
+            }
         }
-        map.put(current_sum,map.getOrDefault(current_sum,0)+1);
-       
+        }
+           return count;
     }
-     return count;
-    }
-}
-//     int n=nums.length;
-//     int count=0;
-//     for(int i=0;i<n;i++){
-//         int sum =0;
-    
-//     for(int j=i;j<n;j++){
-//         sum=sum+nums[j];
-//         if(sum==k){
-//             count++;
-//         }
-//     }
-//     }
+ 
 
-//    return count++;
+}
+//             HashMap<Integer,Integer>map=new HashMap<>();
+//  int n =nums.length;
+
+//  int count=0;
+//  int curr_sum=0;
+//  map.put(0,1);
+//  for(int i=0;i<n;i++){
+//     curr_sum+=nums[i];
+   
+//     if(map.containsKey(curr_sum-k)){
+//         count+=map.get(curr_sum-k);
 //     }
+//     map.put(curr_sum,map.getOrDefault ( curr_sum,0)+1);
+//  }
+//  return count;
 // }
+// }
+// //     int n=nums.length;
+// //     int count=0;
+// //     for(int i=0;i<n;i++){
+// //         int sum =0;
+    
+// //     for(int j=i;j<n;j++){
+// //         sum=sum+nums[j];
+// //         if(sum==k){
+// //             count++;
+// //         }
+// //     }
+// //     }
+
+// //    return count++;
+// //     }
+// // }
