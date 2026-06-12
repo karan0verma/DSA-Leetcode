090ada0582c50758f74f5bf69acce9840936130c
Karan <karanverma07320@gmail.com>
2026-06-12 20:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (64.28%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,17 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
-       int low=0;
-       int high=nums.length-1;
-
-       while(low<=high){
+      int low=0;
+      int high=nums.length-1;
+      while(low<=high){
         int mid= low+((high-low)/2);
-        if(nums[mid]==target)
-        {
+        if(nums[mid]==target){
             return mid;
         }
-        if(nums[low]<=nums[mid])
-        {
-           if(nums[low]<=target && target<nums[mid]){
+        if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<nums[mid]){
                 high=mid-1;
             }
             else{
@@ -19,15 +16,14 @@ class Solution {
             }
         }
         else{
-            if(nums[mid]<target && target<=nums[high]){
+            if(nums[mid]<target&& target<=nums[high]){
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
         }
-        
-       }
-       return -1;
+      }
+      return -1;
     }
 }
